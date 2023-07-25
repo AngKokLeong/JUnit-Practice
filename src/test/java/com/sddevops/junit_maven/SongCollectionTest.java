69f81b871a8e20eb657ebd4467ba304028bacd84
Ang Kok Leong <53397893+AngKokLeong@users.noreply.github.com>
2023-07-25 09:25:27 +0000

    added sortSongsByTitle unit test

--- src/test/java/com/sddevops/junit_maven/SongCollectionTest.java
+++ src/test/java/com/sddevops/junit_maven/SongCollectionTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -130,4 +131,24 @@ public class SongCollectionTest {
 
         assertEquals(song, null);
     }
+
+    @Test
+    void testSortSongsByTitle(){
+        List<Song> songList = songCollection.sortSongsByTitle();
+        
+        List<Song> testSongList = new ArrayList<Song>();
+
+        Song s1 = new Song("001", "good 4 u", "Olivia Rodrigo", 3.59);
+        Song s2 = new Song("002", "Peaches", "Justin Bieber", 3.18);
+        Song s3 = new Song("003", "MONTERO", "Lil Nas", 2.3);
+        Song s4 = new Song("004", "bad guy", "billie eilish", 3.14);
+
+        testSongList.add(s3);
+        testSongList.add(s2);
+        testSongList.add(s4);
+        testSongList.add(s1);
+
+        assertEquals(songList, testSongList);
+    }
+
 }
